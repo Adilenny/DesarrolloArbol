c00b2c87e9f2f9d7afb833f26ce53165ba2cd7d4
jazmin <jazz.min.cas397@gmail.com>
2017-12-11 12:16:29 -0600

    Subir

--- src/sample/Metodos.java
+++ src/sample/Metodos.java
@@ -1,49 +1,36 @@
 package sample;
 
+import java.util.ArrayList;
+
 /**
  * Created by asus1 on 11/29/2017.
  */
 public class Metodos {
+    private ArrayList<Integer> array = new ArrayList<Integer>();
 
     public void PreOrden(NodoArbol raiz){
-        if(raiz != null){
-            System.out.println(raiz.getValor() + " ");
-            PreOrden(raiz.getNodoIzquierdo());
-            PreOrden(raiz.getNodoDerecho());
+        if(raiz != null){ // creo una condición en a cual se coloca la raiz igual a null
+            System.out.println(raiz.getValor() + " ");// mando a imprimier el valor de la raiz
+            PreOrden(raiz.getNodoIzquierdo()); // se manda a traer la raiz para que invoque al valor de nodo izquierdo
+            PreOrden(raiz.getNodoDerecho()); // se manda a traer la raiz para mandar a traer el valor del nodo derecho
         }
     }
 
     public void InOrden(NodoArbol raiz){
-        if (raiz != null){
-            InOrden(raiz.getNodoIzquierdo());
-            System.out.println(raiz.getValor() + " " );
-            InOrden(raiz.getNodoDerecho());
+        if (raiz != null){// se crea una condición en la cual la raiz es igual a null
+            InOrden(raiz.getNodoIzquierdo());// se manda a traer el valor de la raiz, para seguir con el valor de lado izquierdo
+            System.out.println(raiz.getValor() + " " );// se manda a imprimir la raiz
+            InOrden(raiz.getNodoDerecho()); // se manda a traer la raiz para poder mandar a traer el valor del nodo derecho
         }
     }
 
     public void PostOrden(NodoArbol raiz){
-        if (raiz != null){
-            PostOrden(raiz.getNodoIzquierdo());
-            PostOrden(raiz.getNodoDerecho());
-            System.out.println(raiz.getValor() + " ");
+        if (raiz != null){ // Se crea la condición if  se coloca la rama igual a null
+            PostOrden(raiz.getNodoIzquierdo());  // se manda a traer la rama para tomar el valor del nodo izquierdo
+            PostOrden(raiz.getNodoDerecho()); // se manda a llamar la rama para tomar el valor del nodo derecho
+            System.out.println(raiz.getValor() + " "); // se manda a imprimir la raiz
         }
     }
 
-    public String buscarNodo(int d){
-        NodoArbol raiz = null;
-        NodoArbol aux=raiz;
-        while(aux.getDato() !=d){
-            if(d< aux.getDato()){
-                aux=aux.nodoIzquierdo;
-            }else{
-                aux=aux.nodoDerecho;
-
-            }
-            if(aux==null){
-                return d + "No fue encontrado";
-            }
-        }
-        return d + "fue encontrado";
-    }
 
 }
